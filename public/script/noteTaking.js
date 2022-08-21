3f5c5977234ba18354bc5fa8e8e124e08ac02c7b
Daniel.K <danielkaplan812@gmail.com>
2022-08-20 20:09:18 -0400

    Backspace works
    -Backspace works
    -Made NoteCount = to the amount of spans in testNotes.
    -Added a class to only the last span on the page.
    -Need work on error handling

--- public/script/noteTaking.js
+++ public/script/noteTaking.js
@@ -1,55 +1,72 @@
 
-let arrNotes = [];
-const unTypeables = ['Backspace', 'Enter', 'Tab', 'CapsLock', 'Shift', 'Control', 'Alt', 'PageUp', 'PageDown', 'Home', 'Insert', 'End',
-  'Delete', 'Escape', 'Pause', 'ScrollLock', 'ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9',
-  'F10', 'F11', 'F12', 'AudioVolumeUp', 'AudioVolumeDown', 'NumLock', 'Meta', 'ContextMenu']
+// const unTypeables = ['Backspace', 'Enter', 'Tab', 'CapsLock', 'Shift', 'Control', 'Alt', 'PageUp', 'PageDown', 'Home', 'Insert', 'End',
+//   'Delete', 'Escape', 'Pause', 'ScrollLock', 'ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9',
+//   'F10', 'F11', 'F12', 'AudioVolumeUp', 'AudioVolumeDown', 'NumLock', 'Meta', 'ContextMenu']
+
+const testNotes = document.querySelector("#testNotes");
+const notesDiv = document.querySelector("#notesDiv");
 
 let noteCount = 0;
 
-const testNotes = document.querySelector("#testNotes");
-const notesDiv = document.querySelector("#notesDiv")
-const notesArea = document.querySelector("#notesArea");
 document.addEventListener("keydown", async function (evt) {
   let eventKey = evt.key;
-  
-  if (evt.code == 'Backspace') {
-    document.getElementById(`noteCount:${noteCount}`).remove(); 
-    noteCount--;   
-  }
+  noteCount = testNotes.children.length;
 
-  noteCount++;
-  const spanNotes = document.createElement('span')
-  testNotes.append(spanNotes); 
-  spanNotes.setAttribute('id',`noteCount:${noteCount}`)
-  if (eventKey.length === 1) {
+  if (eventKey.length === 1 && eventKey !== ' ') {
+    const spanNotes = document.createElement('span');
+    spanNotes.setAttribute('id', `noteCount:${noteCount}`); //Notecount can break if someone changes the notecount manually
+    testNotes.append(spanNotes);
+    // noteCount++;
     spanNotes.append(eventKey);
+    document.getElementById(`noteCount:${noteCount}`).classList.add('startTypeChar');
+    if(document.getElementById(`noteCount:${noteCount - 1}`) !== null) {
+      document.getElementById(`noteCount:${noteCount - 1}`).classList.remove('startTypeChar');
+    } 
+    else {
+      //Error handling
+    } 
   }
-  
-  // arrNotes.push(eventKey);
-  // for (let i = 0; i < unTypeables.length; i++) {
-  //   arrNotes = arrNotes.filter(evt => evt !== unTypeables[i]);
-  // }
-  // arrNotes = evt.code == 'Backspace' ? testNotes.textContent = arrNotes.substring(lastCharOfNote, notes.length - 1) : "";
-  // let notes = arrNotes.join('');
+  if (evt.code == 'Backspace') {
+    //Needs error handling: For(Uncaught (in promise) TypeError: Cannot read properties of null (reading 'classList'))
+    if(noteCount !== 1) {
+      document.getElementById(`noteCount:${noteCount - 2}`).classList.add('startTypeChar');
+    } else {
+      document.getElementById(`noteCount:${noteCount - 1}`).classList.add('startTypeChar');
+    }
+    noteCount--;
+    testNotes.removeChild(document.getElementById(`noteCount:${noteCount}`));
+  }
+  if (eventKey === ' ' || eventKey === 'Spacebar') {
+    noteCount--;
+    document.getElementById(`noteCount:${noteCount}`).append(' ')
+    // ' ' is standard, 'Spacebar' was used by IE9 and Firefox < 37
+    console.log('Space pressed');
+  }
 
+  if (evt.code == 'ArrowLeft') {
+    console.log(noteCount);
+    // noteCount -= 2;
+    console.log(noteCount);
+  }
 
   if (evt.code == 'Enter') {
     const newLi = document.createElement('ul')
     testNotes.append(newLi);
   }
-  
-  // let lastCharOfNote = notes[notes.length - 1];
-    // console.log(lastCharOfNote);
-    // localStorage.removeItem(lastCharOfNote);
-    // let deletedNote = notes.substring(lastCharOfNote, notes.length - 1);
-    // storedNotes = localStorage.setItem(deletedNote); 
-    // localStorage.setItem("storedNotes", deletedNote);
-    // let newNotes = notes.substring(0, notes.length - 1);
-    // testNotes.textContent -= lastCharOfNote;
-  // testNotes.append(notes); 
-  // localStorage.setItem("storedNotes", notes);
-  // testNotes.textContent = localStorage.getItem('storedNotes');
+  // console.log(noteCount);
 })
+
+
+
+
+
+
+
+
+
+
+
+
 
 // function insertAtCursor(myField, myValue) {
 //   //IE support
