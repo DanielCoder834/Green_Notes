3bf44f04cc9b3367e705c15f28404f648302d90a
Daniel.K <danielkaplan812@gmail.com>
2022-08-28 20:51:44 -0400

    3 days left

--- public/script/noteTaking.js
+++ public/script/noteTaking.js
@@ -5,15 +5,20 @@
 
 // const e = require("express");
 
+// const e = require("express");
+
 const testNotes = document.querySelector("#testNotes");
 const notesDiv = document.querySelector("#notesDiv");
 
 let noteCount = 0;
 let movedPlaceForText = false;
 let enterPressed = false;
+let clickedOn = false; 
+let controlPressed = false; 
 
 document.addEventListener("keydown", async function (evt) {
   let eventKey = evt.key;
+
   // noteCount = testNotes.getElementsByTagName('span').length;
   //Old Way
   // noteCount = testNotes.children.length;
@@ -21,7 +26,7 @@ document.addEventListener("keydown", async function (evt) {
   let lastSpanNote = document.querySelector(`[data-noteid="${noteCount - 1}"]`)
   let spanNoteTypeChar = document.querySelector(`.startTypeChar`);
 
-  if (eventKey.length === 1 && eventKey !== ' ') {
+  if (eventKey.length === 1 && eventKey !== ' ' && controlPressed == false) {
     // Adds a cool effect
     // const spanCursor = document.createElement('span');
     // spanCursor.classList.add('blinkyCursor'); 
@@ -29,12 +34,15 @@ document.addEventListener("keydown", async function (evt) {
     const spanNotes = document.createElement('span');
     // spanNotes.setAttribute('data-noteid', `${noteCount}`); //Notecount can break if someone changes the notecount manually
     if (enterPressed == false) {
-      testNotes.append(spanNotes); //Issue when pressing Enter because testNotes
+      if(document.querySelectorAll(`.startTypeChar`).length <= 0) testNotes.append(spanNotes);
+      else document.querySelector(`.startTypeChar`).insertAdjacentElement("afterend", spanNotes);
       // Adds a cool effect
       // testNotes.append(spanCursor);
     }
     else if (enterPressed) {
-      document.querySelector('.enterPress').append(spanNotes);
+      document.querySelector(`.startTypeChar`).insertAdjacentElement("afterend", spanNotes);
+      //Old Way
+      // document.querySelector('.enterPress').append(spanNotes);
       // Adds a cool effect
       // document.querySelector('.enterPress').append(spanCursor);
     }
@@ -48,15 +56,15 @@ document.addEventListener("keydown", async function (evt) {
       spanNotes.textContent = eventKey;
       // Adds a cool effect
       // spanNoteTypeChar.insertAdjacentElement("afterend", spanCursor);
-      console.log('hi');
     }
     else if (testNotes.getElementsByTagName('span').length !== 0 && movedPlaceForText == false) {
       for (let i = 0; i < testNotes.getElementsByTagName('span').length; i++) {
         testNotes.getElementsByTagName('span')[i].setAttribute('data-noteid', `${i}`);
       }
+      if(clickedOn) document.querySelector('.startTypeChar').append(eventKey);
       document.querySelector('.startTypeChar').classList.remove('startTypeChar');
       document.querySelector(`[data-noteid="${testNotes.getElementsByTagName('span').length - 1}"]`).classList.add('startTypeChar');
-      document.querySelector('.startTypeChar').append(eventKey);
+      if(!clickedOn) document.querySelector('.startTypeChar').append(eventKey); 
     }
     else if (movedPlaceForText) {
       for (let i = 0; i < testNotes.getElementsByTagName('span').length; i++) {
@@ -70,6 +78,15 @@ document.addEventListener("keydown", async function (evt) {
     }
     else {
       //Error Handling
+    }
+  }
+  if(evt.ctrlKey && eventKey === 'b') {
+    controlPressed = true;
+    // 
+    let currentNotes = document.querySelectorAll(testNotes.children.hasAttribute('`[data-noteid=0]'));
+    if(currentNotes.matches(':hover')) {
+      console.log('ds');
+      document.querySelector(`.startTypeChar`).classList.add('bolder'); 
     }
   }
   if (eventKey === ' ' || eventKey === 'Spacebar') {
@@ -107,7 +124,7 @@ document.addEventListener("keydown", async function (evt) {
     movedPlaceForText = true;
   }
   if (evt.code == 'Enter') {
-    
+
     // document.querySelector('.startTypeChar').classList.remove('startTypeChar'); //This Line needs to be fixed
     // const spanNotes = document.createElement('span');
     // spanNotes.setAttribute('data-noteid', `${noteCount}`); //Notecount can break if someone changes the notecount manually
@@ -115,10 +132,10 @@ document.addEventListener("keydown", async function (evt) {
 
     const newUl = document.createElement('ul');
     const newLi = document.createElement('li');
-    if(document.querySelector('.enterPress') !== null) document.querySelector('.enterPress').classList.remove('enterPress');
+    if (document.querySelector('.enterPress') !== null) document.querySelector('.enterPress').classList.remove('enterPress');
     newLi.classList.add('enterPress');
 
-    if(enterPressed == false) {
+    if (enterPressed == false) {
       testNotes.append(newUl);
       console.log('ddsds');
     }
@@ -127,7 +144,7 @@ document.addEventListener("keydown", async function (evt) {
     }
     document.querySelector(`[data-ulnoteid="${0}"]`).append(newLi);
     // newLi.append(spanNotes);
-    const tempSpanNote = document.createElement('span'); 
+    const tempSpanNote = document.createElement('span');
     newLi.append(tempSpanNote);
     for (let i = 0; i < testNotes.getElementsByTagName('li').length; i++) {
       testNotes.getElementsByTagName('li')[i].setAttribute('data-linoteid', `${i}`);
@@ -146,7 +163,7 @@ document.addEventListener("keydown", async function (evt) {
       if (enterPressed == false) {
         selectedElmBackspace.classList.remove('startTypeChar'); //This Line needs to be fixed
         selectedElmBackspace.previousSibling.classList.add('startTypeChar');
-        testNotes.removeChild(selectedElmBackspace); 
+        testNotes.removeChild(selectedElmBackspace);
       }
       else if (enterPressed == true) {
         if (selectedElmBackspace.previousElementSibling !== null) {
@@ -157,10 +174,10 @@ document.addEventListener("keydown", async function (evt) {
         else if (selectedElmBackspace.previousSibling == null) {
           console.log(document.querySelector(`.startTypeChar`));
           document.querySelector(`.enterPress`).remove();
-          if(testNotes.getElementsByTagName('li').length !== 0) document.querySelector(`[data-linoteid="${testNotes.getElementsByTagName('li').length - 1}"]`).classList.add('enterPress')
+          if (testNotes.getElementsByTagName('li').length !== 0) document.querySelector(`[data-linoteid="${testNotes.getElementsByTagName('li').length - 1}"]`).classList.add('enterPress')
           // testNotes.removeChild(document.querySelector(`[data-ulnoteid="0"]`));
           document.querySelector(`[data-noteid="${testNotes.getElementsByTagName('span').length - 1}"]`).classList.add('startTypeChar');
-          if(testNotes.getElementsByTagName('li').length == 0) {
+          if (testNotes.getElementsByTagName('li').length == 0) {
             enterPressed = false;
             testNotes.removeChild(document.querySelector(`[data-ulnoteid="${testNotes.getElementsByTagName('ul').length - 1}"]`));
           }
@@ -185,7 +202,10 @@ document.addEventListener("click", async function (evt) {
   if (testNotes.contains(evt.target)) {
     document.querySelector('.startTypeChar').classList.remove('startTypeChar'); //Try to add a querySelectorAll without errors
     evt.target.classList.add('startTypeChar');
-    console.log(evt.target);
+    console.log(enterPressed);
+    document.querySelector('.enterPress').classList.remove('enterPress');
+    evt.target.parentElement.classList.add('enterPress');
+    clickedOn = true; 
   } else {
     console.log('not the target');
   }
